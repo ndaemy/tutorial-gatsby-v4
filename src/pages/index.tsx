01ce50fbfe2d9ba38265bf73f296e9a5eb5e8805
ndaemy <yuyaebean@gmail.com>
2022-03-08 22:16:46 +0900

    ✨ 3. Add Features with Plugins

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from "react";
 import Layout from "../components/Layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+        src="../images/dog.jpg"
+      />
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
