96202b87b843ff639ff32832facd052ed83fea49
ndaemy <yuyaebean@gmail.com>
2022-03-15 23:07:55 +0900

    ✨ 7. Add Dynamic Images from Data

--- src/pages/blog/{mdx.slug}.tsx
+++ src/pages/blog/{mdx.slug}.tsx
@@ -1,22 +1,36 @@
 import React from "react";
 import { graphql, PageProps } from "gatsby";
-import Layout from "../../components/layout";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { GatsbyImage, getImage, ImageDataLike } from "gatsby-plugin-image";
+import Layout from "../../components/layout";
 
 type DataProps = {
   mdx: {
     frontmatter: {
       title: string;
       date: string;
+      hero_image_alt: string;
+      hero_image_credit_link: string;
+      hero_image_credit_text: string;
+      hero_image: ImageDataLike;
     };
     body: string;
   };
 };
 
 const BlogPost: React.FC<PageProps<DataProps>> = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image);
+
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage alt={data.mdx.frontmatter.hero_image_alt} image={image!} />
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
   );
@@ -27,7 +41,15 @@ export const query = graphql`
     mdx(id: { eq: $id }) {
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
       body
     }
